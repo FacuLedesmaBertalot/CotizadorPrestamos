3b291efea83f4c4445bb291c0fffc55cce13809d
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-21 18:27:38 -0300

    Agregado funcion de total a pagar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import Header from "./components/Header"
 import Button from "./components/Button"
-import { formatearDinero } from './helpers';
+import { formatearDinero, calcularTotalPagar } from './helpers';
 
 function App() {
   const [cantidad, setCantidad] = useState(10000);
-
   const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
 
   const MIN = 0;
   const MAX = 20000;
@@ -83,6 +83,15 @@ function App() {
           <option value="12">12 Meses</option>
           <option value="24">24 Meses</option>
         </select>
+
+        <div className='my-5 space-y-3 bg-gray-50 p-5'></div>
+          <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+            Resumen <span className='text-indigo-600'>de Pagos</span> 
+          </h2>
+
+          <p className='text-xl text-gray-500 text-center font-bold'>{meses} Meses</p>
+          <p className='text-xl text-gray-500 text-center font-bold'>Total a Pagar</p>
+          <p className='text-xl text-gray-500 text-center font-bold'>Mensuales</p>
     </div>
   )
 }
